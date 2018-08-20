85631c5dc2b82ce3d12a8ef4d6db24425c8b02c1
Hansel Tertius <hansel.tertius@my.jcu.edu.au>
2018-08-20 17:24:05 +0700

    Implement an example functionality for displaying text

--- app/src/main/java/com/example/android/borobudurtour/HistoryFragment.java
+++ app/src/main/java/com/example/android/borobudurtour/HistoryFragment.java
@@ -7,10 +7,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class HistoryFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_history, container, false);
+
+        View rootView =  inflater.inflate(R.layout.fragment_history, container, false);
+
+        TextView textOne = (TextView) rootView.findViewById(R.id.word_one);
+        textOne.setText(R.string.history_section);
+
+        TextView textTwo = (TextView) rootView.findViewById(R.id.word_two);
+        textTwo.setText(R.string.history_section_content);
+
+        return rootView;
     }
 }
