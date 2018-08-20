1f007bbd59ffb3015bedc4b636a31845defc84ff
Hansel Tertius <hansel.tertius@my.jcu.edu.au>
2018-08-20 11:54:26 +0700

    Add HistoryActivity for covering History section

---
+++ app/src/main/java/com/example/android/borobudurtour/HistoryActivity.java
@@ -0,0 +1,17 @@
+package com.example.android.borobudurtour;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+
+        // Find the ViewPager id to be able to swipe between fragments
+        ViewPager viewPager = (ViewPager) findViewById(R.id.historyViewPager);
+    }
+}
